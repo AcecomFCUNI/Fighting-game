78f2383834cb396617091b9d3009c3dcddea0ed3
Christian Poma Navarro <christian.poma.n@uni.pe>
2024-01-28 12:43:26 -0500

    jump code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,41 +1,115 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
+{   
     [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
 
     private float xAxis;
 
     private Rigidbody2D rb;
     private Animator an;
+    public bool isJump;
+    public bool grounded;
+    private float jumpHeight;
+    [SerializeField] private double maxJumpHeight;
+    private Vector2 jumpVelocity;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         an = GetComponent<Animator>();
+        grounded = true;
+        isJump = false;
     }
 
     void Update()
     {
+        ReadInputs();
         Move();
         Run();
     }
+
+    private void FixedUpdate()
+    {
+        Jump();
+        HandleGravity();
+    }
+
+    private void HandleGravity()
+    {
+        if (!grounded)
+        {
+            if (transform.position.y >= maxJumpHeight)
+            {
+                var velocity = rb.velocity;
+                velocity = new Vector2(velocity.x, -1*velocity.y);
+                rb.velocity = velocity;
+            }
+
+            // if (Math.Abs(transform.position.y - (maxJumpHeight - 2f)) < 0.05f)
+            // {
+            //     if(rb.velocity.y < 0) rb.velocity = jumpVelocity;
+            //     else
+            //     {
+            //         var velocity = rb.velocity;
+            //         velocity = new Vector2(velocity.x, -1*velocity.y);
+            //         jumpVelocity = velocity;
+            //         velocity = new Vector2(velocity.x, 1f);
+            //         rb.velocity = velocity;    
+            //     }
+            //     
+            // }
+
+        }
+    }
+
     private void LateUpdate()
     {
         if (rb.velocity.x != 0) an.SetBool("isMoving", true);
         else an.SetBool("isMoving", false);
         an.SetFloat("xAxis", xAxis);
     }
-    private void Move()
+
+    private void ReadInputs()
     {
         xAxis = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJump = true;
+        }
+    }
+    
+    private void Move()
+    {
         rb.velocity = new Vector2(xAxis * speed, rb.velocity.y);
     }
 
     private void Run()
     {
         
+    }
+    
+    private void Jump()
+    {
+        if (grounded && isJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            grounded = false;
+            isJump = false;
+        } 
+        
+    }
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJump = false;
+            grounded = true;
+        }
     }
 }
